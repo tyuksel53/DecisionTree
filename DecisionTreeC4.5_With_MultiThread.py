a038d968dc039bd627d9deba16138b1b550b27c0
Taha Yüksel <taha.yuksel53@hotmail.com>
2017-04-05 01:25:14 +0300

    Entropy  finding
    winter is coming

--- DecisionTreeC4.5_With_MultiThread.py
+++ DecisionTreeC4.5_With_MultiThread.py
@@ -3,47 +3,71 @@ clr.AddReference('System.Drawing')
 clr.AddReference('System.Windows.Forms')
 
 from Django import Django
+import ctypes 
+import os
 from System.Drawing import *
 from System.Windows.Forms import Application, Button, Form, Label,OpenFileDialog,TextBox
 
 class MyForm(Form):
     def __init__(self):
         # Create child controls and initialize form
-        self.Text = 'Decision Tree'
-        self.Name = 'Form'
-        self.Width = 512
-        self.Height = 512
-        self.CenterToScreen()
+        try:
+            self.Text = 'Decision Tree'
+            self.Name = 'Form'
+            self.Width = 430
+            self.Height = 200
+            self.CenterToScreen()
 
-        self.TextBox = TextBox()
-        self.TextBox.Text = ""
-        self.TextBox.Location = Point(50, 50)
-        self.TextBox.Width = 250
+            self.TextBox = TextBox()
+            self.TextBox.Text = ""
+            self.TextBox.Location = Point(20, 50)
+            self.TextBox.Width = 250
         
-        button = Button()
-        button.Text = "Choose File"
-        button.Location = Point(49, 75)
-        button.Click += self.buttonPressed
+            self.LabelAuthors = Label()
+            self.LabelAuthors.Text = "Taha Yuksel - Ramazan Demir"
+            self.LabelAuthors.Location = Point(300, 50)
+            self.LabelAuthors.Height = 100
+            self.LabelAuthors.Width = 100
 
-        self.Controls.Add(button)
-        self.Controls.Add(self.TextBox)
+            buttonFile = Button()
+            buttonFile.Text = "Choose File"
+            buttonFile.Location = Point(19, 75)
+            buttonFile.Click += self.buttonPressed
 
-        self.count = 0
-        self.FilePath = ""
+            buttonProcess = Button()
+            buttonProcess.Text = "Start Process"
+            buttonProcess.Location = Point(195, 75)
+            buttonProcess.Click += self.buttonProcessStart
+
+            self.Controls.Add(buttonFile)
+            self.Controls.Add(self.TextBox)
+            self.Controls.Add(buttonProcess)
+            self.Controls.Add(self.LabelAuthors)
+
+            self.FilePath = ""
+        except Exception as ex:
+            print ex
+            raw_input()
+    def buttonProcessStart(self,sender,args):
+        #clear = lambda: os.system('cls')
+        #clear()
+        if len(self.FilePath)<5:
+            ctypes.windll.user32.MessageBoxW(0, "WOWOOWOWOW you must chose the file", "THE FILE", 1)
+        else:
+            DjangoUnchained = Django(self.FilePath)
+            DjangoUnchained.findAllEntropy()
 
     def buttonPressed(self, sender, args):
         #try:  
         #except Exception as ex:
             #print ex
             #raw_input()
-        DjangoUnchained = Django()
         dialog = OpenFileDialog()
         dialog.Filter = "All Files|*.*"
         if dialog.ShowDialog() == True:
            pass
         self.FilePath = dialog.FileName
         self.TextBox.Text = dialog.FileName
-        DjangoUnchained.findEntropy(self.FilePath)
         
         
 
--- Django.py
+++ Django.py
@@ -1,29 +1,144 @@
+import math
 class Django:
-    def findEntropy(self,path):
+
+    generalEntropy=0    ## general_entropy
+    global columnSize   ## how many columns in file
+    filePath = ""       ## file path
+    fileRow = 0         ## file row counter
+    ColumnEntropys = []
+    def __init__(self,path): ## class constructor
         try:
-            file = open(path, "r")
-            VariableInResult = []
-            VariableInResultCounter = []
+            self.filePath = path ## adding file path
+            self.GetFuncsize()   ## adding columnsize
+            self.generalEntropy = self.findGeneralEntropy() ## finding general entropy
+        except Exception as ex:
+            print ex
+            raw_input()
+
+    def GetFuncsize(self):
+        try:
+            file = open(self.filePath, "r")
+            line = file.readline()
+            line = line.split(',')
+            self.columnSize = len(line) ## Initialize data columnsize
+        except Exception as ex:
+            print ex
+            raw_input()
+
+    def EntropyFormula(self,total,current): ## EntropyFormula definition
+        dummy = current/ float(total)
+        dummy = dummy * math.log(dummy, 2)
+        return dummy * -1
+    
+    def CompareZ(self,columnNum,columnValue):
+        try:
+            file = open(self.filePath, "r")
+            VariableInResult = [] ## add variables to list
+            VariableInResultCounter = [] ## this for count variables
             Count = 0
             Control = 0
-            i =0
+            i=0
+            RowCount = 0
+            while True:
+                  line = file.readline()
+                  if line == '': ## if it's at the end of file
+                     break
+                  line = line.split(',')
+                  if line[columnNum] == columnValue: ## find specific Area
+                      for i  in range(len(VariableInResult)): ## check variable is already added
+                          if VariableInResult[i] == line[len(line)-1]: ## if variable is already in the list
+                             VariableInResultCounter[i] +=1 ## count up 
+                             Control = 1 ## variable is finded
+                      if Control == 0: ## if variable is not finded then add variable
+                         VariableInResult.append(line[len(line)-1])
+                         VariableInResultCounter.append(1)
+                      Control = 0
+                      RowCount += 1
+            total=0
+            for i  in range(len(VariableInResult)):
+                      total += self.EntropyFormula(RowCount,VariableInResultCounter[i])
+            return total
+        except Exception as ex:
+            print ex
+            raw_input()
+    def findAllEntropy(self): ## finding all column's entropyes
+        try:
+            for i in range(self.columnSize-1):
+                self.findEntropy(self.columnSize - i)
+                print "_______________________________________________________________"
+
+            print "_______________________________________________________________"
+            print self.generalEntropy
+            print "_______________________________________________________________"
+            for i in range(len(self.ColumnEntropys)):
+                natay = self.generalEntropy -  self.ColumnEntropys[i]
+                print "_______________________________________________________________"
+                print  natay
+        except Exception as ex:
+            print ex
+            raw_input()
+    def findEntropy(self,ColumnNum): ## finding entropy column by column
+        try:
+            file = open(self.filePath, "r")
+            VariableInResult = []
+            VariableInResultCounter = []
+            VariableInZColumn = []
+            VariableInZColumnCounter = []
+            Count = 0
+            Control = 0
+            i,t,m =0,0,0
             while True:
                   line = file.readline()
                   if line == '':
                      break
                   line = line.split(',')
+                  line = [s.replace('\n', '') for s in line]
                   for i  in range(len(VariableInResult)):
-                      if VariableInResult[i] == line[len(line)-4]:
+                      if VariableInResult[i] == line[len(line)-ColumnNum]:
                          VariableInResultCounter[i] +=1
                          Control = 1
                   if Control == 0:
-                     VariableInResult.append(line[len(line)-4])
+                     VariableInResult.append(line[len(line)-ColumnNum])
                      VariableInResultCounter.append(1)
                   Control = 0
+            TotalEntropy = 0
             for i  in range(len(VariableInResult)):
-                      print VariableInResult[i]," ",  VariableInResultCounter[i]
+                a = ( VariableInResultCounter[i] / float(self.fileRow) );
+                b = self.CompareZ(self.columnSize-ColumnNum,VariableInResult[i])
+                dummy = a *b
+                TotalEntropy = dummy + TotalEntropy
+                print VariableInResult[i]," ",  VariableInResultCounter[i], " ",dummy
+            self.ColumnEntropys.append(TotalEntropy)
         except Exception as ex:
             print ex
             raw_input()
-        
-
+    def findGeneralEntropy(self): ## finding general entropy
+        try:
+            file = open(self.filePath, "r")
+            VariableInResult = [] ## add variables to list
+            VariableInResultCounter = [] ## this for count variables
+            Count = 0
+            Control = 0
+            i=0
+            while True:
+                  line = file.readline()
+                  if line == '': ## if it's at the end of file
+                     break
+                  line = line.split(',')
+                  for i  in range(len(VariableInResult)): ## check variable is already added
+                      if VariableInResult[i] == line[len(line)-1]: ## if variable is already in the list
+                         VariableInResultCounter[i] +=1 ## count up 
+                         Control = 1 ## variable is finded
+                  if Control == 0: ## if variable is not finded then add variable
+                     VariableInResult.append(line[len(line)-1])
+                     VariableInResultCounter.append(1)
+                  Control = 0
+                  self.fileRow += 1 ## file row counter
+            total=0
+            for i  in range(len(VariableInResult)):
+                      total += self.EntropyFormula(self.fileRow,VariableInResultCounter[i])
+            return total
+        except Exception as ex:
+            print ex
+            raw_input()
+    
